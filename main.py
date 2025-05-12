6bf2f949090e5124a9d54fa473fe594b41ff0b98
Lxtharia <maud-lin@gmx.de>
2025-05-12 21:40:54 +0200

    Persist last_sent_at; Handle pagination

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from time import sleep
 
 import os
 from atproto_client.models.app.bsky.feed.defs import FeedViewPost, PostView
+from atproto_client.models.app.bsky.feed.get_feed import Params
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+LAST_SENT_AT_FILENAME = "last_sent_at.txt"
 
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 TIMEOUT = 2
-LAST_SENT_TIMESTAMP = 0
 
 
 class Post:
@@ -30,26 +32,51 @@ class Post:
                 '{self.content}' with {"No" if self.embeds is None else ""} embeds>"""
 
 
-def process_user_posts(client: Client, webhook: str | None, user_did: str, last_sent) -> list[Post]:
-    next_handle = user_did
-    while next_handle is not None:
+def update_last_sent(time: datetime):
+    with open(LAST_SENT_AT_FILENAME, 'w') as f:
+        f.write(f"last_sent_at={time.timestamp()}")
+
+
+def read_last_sent():
+    timestamp = 0
+    if os.path.exists(LAST_SENT_AT_FILENAME):
+        with open(LAST_SENT_AT_FILENAME, 'r') as f:
+            timestamp = f.read().split(sep='=')[1]
+    return datetime.fromtimestamp(float(timestamp))
+
+
+def process_user_posts(client: Client, webhook: str | None, user_did: str):
+    next_page = None
+    while True:
         data = client.get_author_feed(
-            actor=next_handle,
+            actor=user_did,
+            cursor=next_page,
             filter='posts_and_author_threads',
             limit=30,
         )
         # For paging
-        next_handle = data.cursor
         feed = data.feed
-        for feedpost in feed:
+        next_page = data.cursor
+        last_sent_at = read_last_sent()
+
+        reached_end = False
+        for feedpost in reversed(feed):
             post = Post(feedpost)
+
             # Skip already seen posts
-            if post.time.timestamp() <= datetime.fromtimestamp(last_sent).timestamp():
+            if post.time.timestamp() <= last_sent_at.timestamp():
+                reached_end = True  # No need to paginate further
+                print(f"<Skipping post from @{post.time.timestamp()}>")
                 continue
 
-            post_post(webhook, post)
+            if post_post(webhook, post):
+                update_last_sent(post.time)
+
             sleep(TIMEOUT)
-    return []
+
+        print(next_page)
+        if reached_end or next_page is None:
+            break
 
 
 def did_resolver(username):
@@ -60,7 +87,7 @@ def did_resolver(username):
 def post_post(webhook: str | None, post: Post):
     if webhook is None:
         print(f"Post: {post}")
-        return
+        return True
 
 
 def main():
@@ -73,7 +100,9 @@ def main():
         exit("Errör")
     else:
         mydid = client.me.did
-        process_user_posts(client, WEBHOOK_URL, mydid, LAST_SENT_TIMESTAMP)
+        while True:
+            process_user_posts(client, WEBHOOK_URL, mydid)
+            sleep(60*5)
 
 
 if __name__ == "__main__":
