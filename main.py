6a0937dfa40744f1fc5a4190355ea5dfb30592c2
Lxtharia <maud-lin@gmx.de>
2025-06-30 21:09:12 +0200

    Change embed style; Add profile url; Add ability to dry run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import logging
+import re
 import sys
 from atproto import Client, IdResolver
 from time import sleep
@@ -44,12 +45,15 @@ class Post:
         self.author_handle = post.post.author.handle
         self.author_display_name = post.post.author.display_name
         self.author_avatar = post.post.author.avatar
+        self.author_profile_url = "https://bsky.app/profile/" + self.author_handle
         self.content = post.post.record.text
         self.embeds = post.post.record.embed
+        self.url = atUriToBskyAppUrl(post.post.uri, author_handle=self.author_handle)
         self.time: datetime = datetime.fromisoformat(post.post.indexed_at)
 
     def __format__(self, format_spec: str, /) -> str:
         return f"""<Post by '@{self.author_handle}' at [{self.time.isoformat()}] \
+url: [{self.url}]
 with {"no" if self.embeds is None else ""} embeds:
     {self.content}>"""
 
@@ -109,6 +113,21 @@ def post_posts(posts: list[Post], webhook: str | None):
         sleep(DISCORD_TIMEOUT)
 
 
+def print_posts(posts: list[Post]):
+    for post in reversed(posts):
+        print(f"""
+=== New post from {post.author_display_name} ===
+=== [url: {post.url} | posted at: {post.time}]
+vvv [embeds: {post.embeds}] vvv
+{post.content}
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+""")
+
+
+def optional(key: str, val) -> dict[str, str]:
+    return {key: str(val)} if val is not None else {}
+
+
 def send_post_to_webhook(post: Post, webhook: str | None):
     if webhook is None:
         print(f"{post}")
@@ -120,13 +139,22 @@ def send_post_to_webhook(post: Post, webhook: str | None):
         "avatar_url": "https://web-cdn.bsky.app/static/favicon.png",
         "embeds": [
             {
-                "author": {
-                    "name": post.author_display_name,
-                    "url": "https://bsky.app/profile/" + post.author_handle,
-                    "icon_url": post.author_avatar,
+                "title": f"New Post from {post.author_display_name}",
+                **optional("url", post.url),
+                "thumbnail": {
+                    **optional("url", post.author_avatar),
                 },
+                # "author": {
+                #     "name": post.author_display_name,
+                #     "url": "https://bsky.app/profile/" + post.author_handle,
+                #     "icon_url": post.author_avatar,
+                # },
                 # "description": post.content,
                 "fields": [
+                    {
+                        "name": post.author_display_name,
+                        "value": f"[@{post.author_handle}]({post.author_profile_url})",
+                    },
                     {
                         "name": post.content,
                         "value": "",
@@ -134,12 +162,14 @@ def send_post_to_webhook(post: Post, webhook: str | None):
                 ],
                 "color": 1941746,
                 "footer": {
-                    "text": "@" + post.author_handle,
+                    "text": "bsky.app",
                 },
                 "timestamp": post.time.isoformat(),
             },
         ]
     }
+    print("[DEBUG]", webhook_data)
+
     res = requests.post(url=webhook, json=webhook_data)
     if not res.ok:
         warning(f"Sending webhook failed: {res.content}")
@@ -151,7 +181,33 @@ def did_resolver(username):
     return resolver.handle.resolve(username)
 
 
+def atUriToBskyAppUrl(at_uri: str, author_handle: str | None) -> (str | None):
+    # at://did:plc:2vrama6qvld2tbwuokqvxzxq/app.bsky.feed.post/3lspbv6aq2c2o
+    regex = re.compile('^at://([^/]+)/([^/]+)/([^/]+)$')
+    match = regex.match(at_uri)
+
+    print(f"[DEBUG] at_uri: {at_uri} | matches: {match}")
+
+    if not match:
+        return None  # Invalid AT URI format
+
+    did = author_handle if author_handle is not None else match[1]
+    collection = match[2]
+    rkey = match[3]
+
+    if collection == 'app.bsky.feed.post':
+        return f"https://bsky.app/profile/{did}/post/{rkey}"
+    else:
+        return None  # Not a post record
+
+
 def main():
+
+    dryrun = False
+    if "--dry-run" in sys.argv or "-n" in sys.argv:
+        dryrun = True
+        print("[INFO] Dry-run. No posts will be sent to the webhook")
+
     print("Logging in...")
     client = Client()
     client.login(USERNAME, PASSWORD)
@@ -162,8 +218,11 @@ def main():
     else:
         mydid = client.me.did
         while True:
-            post_posts(fetch_new_user_posts(client, mydid), WEBHOOK_URL)
-            print(" . . . WAITING . . . ")
+            if not dryrun: 
+                post_posts(fetch_new_user_posts(client, mydid), WEBHOOK_URL)
+            else:
+                print_posts(fetch_new_user_posts(client, mydid))
+            print(f"| | | WAITING for {TIMEOUT} minutes... ")
             sleep(60 * TIMEOUT)
 
 
